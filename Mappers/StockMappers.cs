5231060e36c8a6e493554883f8eed98cbb75f707
hotrucdiep21 <hotrucdiep210203@gmail.com>
2024-11-23 09:38:39 +0700

    <feat>: interface, repository

---
+++ Mappers/StockMappers.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using api.Dtos.Stock;
+using api.Models;
+
+namespace api.Mappers
+{
+    public static class StockMappers
+    {
+        public static StockDto ToStockDto(this Stock stockModel)
+        {
+
+            return new StockDto
+            {
+                Id = stockModel.Id,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                LastDev = stockModel.LastDev,
+                Industry = stockModel.Industry,
+                MarketCap = stockModel.MarketCap
+            };
+        }
+
+        public static Stock ToStockFromCreateDTO(this CreateStockRequestDto stockDto)
+        {
+            return new Stock
+            {
+                Symbol = stockDto.Symbol,
+                CompanyName = stockDto.CompanyName,
+                Purchase = stockDto.Purchase,
+                LastDev = stockDto.LastDev,
+                Industry = stockDto.Industry,
+                MarketCap = stockDto.MarketCap,
+
+            };
+        }
+    }
+}
---
+++ Repositorys/StockRepository.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Dtos.Stock;
+using api.Interfaces;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repositorys
+{
+    public class StockRepository : IStockRepository
+    {
+        private readonly ApplicationDBContext _context;
+        public StockRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public Task<List<Stock>> GetAllAsync()
+        {
+            return _context.Stock.ToListAsync();
+        }
+        
+        public async Task<Stock> CreateAsync(Stock stockModel)
+        {
+            await _context.Stock.AddAsync(stockModel);
+            await _context.SaveChangesAsync();
+            return stockModel;
+        }
+
+        public async Task<Stock?> DeleteAsync(int id)
+        {
+            var stockModel = await _context.Stock.FirstOrDefaultAsync(stock => stock.Id == id);
+            if (stockModel == null)
+            {
+                return null;
+            }
+            
+            _context.Stock.Remove(stockModel);
+            await _context.SaveChangesAsync();
+
+            return stockModel;
+        }
+        public async Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto updateDto)
+        {
+            var existingStock = await _context.Stock.FirstOrDefaultAsync(stock => stock.Id == id);
+            if (existingStock == null)
+            {
+                return null;
+            }
+
+            existingStock.Symbol = updateDto.Symbol;    
+            existingStock.CompanyName = updateDto.CompanyName;
+            existingStock.Purchase = updateDto.Purchase;
+            existingStock.LastDev = updateDto.LastDev;
+            existingStock.Industry = updateDto.Industry;
+            existingStock.MarketCap = updateDto.MarketCap;
+
+            await _context.SaveChangesAsync();
+            return existingStock;
+
+        }
+
+        public async Task<Stock?> GetByIdAsync(int id)
+        {
+            var existingStock = await _context.Stock.FindAsync(id);
+            if (existingStock == null)
+            {
+                return null;
+            }
+            return existingStock;
+        }
+    }
+}
